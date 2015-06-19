52ccb217f91435ce72f0f6a511845a51afd179e2
Fulvio Notarstefano <fulvio.notarstefano@gmail.com>
2015-06-19 12:50:04 +0200

    editorconfig, phpdocs edits

--- wp-php-console.php
+++ wp-php-console.php
@@ -18,6 +18,29 @@
  * Domain Path:       /languages
  */
 
+/**
+ * WP PHP Console
+ * Copyright (c) 2014-2015 Fulvio Notarstefano <fulvio.notarstefano@gmail.com>
+ * and contributors
+ *
+ * PhpConsole server library.
+ * Copyright (c) 2011-2013 by Barbushin Sergey <barbushin@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License, version 2 or, at
+ * your discretion, any later version, as published by the Free
+ * Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
 if ( ! defined( 'WPINC' ) ) {
 	exit; // Exit if accessed directly
 }
