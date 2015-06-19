dc596de13d0532a263315a957a879f18ca3b3104
Fulvio Notarstefano <fulvio.notarstefano@gmail.com>
2015-06-19 13:41:33 +0200

    updated phpdocs

--- wp-php-console.php
+++ wp-php-console.php
@@ -3,14 +3,14 @@
  * @link              https://github.com/nekojira/wp-php-console/
  * @since             1.0.0
  * @package           WP_PHP_Console
- * @author            nekojira <fulvio@nekojira.com>
+ * @author            Fulvio Notarstefano <fulvio.notarstefano@gmail.com>
  *
  * @wordpress-plugin
  * Plugin Name:       WP PHP Console
  * Plugin URI:        https://github.com/nekojira/wp-php-console/
  * Description:       An implementation of PHP Console for WordPress. Easily debug and trace PHP errors and warnings from your Chrome dev tools console using a Google Chrome extension.
  * Version:           1.3.5
- * Author:            nekojira
+ * Author:            Fulvio Notarstefano
  * Author URI:        https://github.com/nekojira/
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
