92d91e54674d7c2207c8b592f653560d3237de2d
Fulvio Notarstefano <fulvio.notarstefano@gmail.com>
2018-05-08 23:46:10 +0800

    log in as admin

--- tests/acceptance/AdminTestCest.php
+++ tests/acceptance/AdminTestCest.php
@@ -7,16 +7,17 @@ class AdminTestCest  {
 
 		$I->wantTo( 'See if WP PHP Console is listed in the Plugins page.' );
 
+		$I->loginAsAdmin();
 		$I->amOnPluginsPage();
 
-		$I->seePluginActivated( 'hello-dolly' );
-
-		$I->seePluginDeactivated( 'wp-php-console' );
+		$I->seePluginInstalled( 'wp-php-console' );
 
 		$I->wantTo( 'Deactivate and reactivate WP PHP Console.' );
 
-		//$I->deactivatePlugin( 'wp-php-console' );
+		$I->deactivatePlugin( 'wp-php-console' );
+		$I->seePluginDeactivated( 'wp-php-console' );
 		$I->activatePlugin( 'wp-php-console' );
+		$I->seePluginActivated( 'wp-php-console' );
 	}
 
 
