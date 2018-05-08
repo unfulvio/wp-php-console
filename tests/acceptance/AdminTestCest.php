354352bba648a11eaea08154222d0ba9de084af0
Fulvio Notarstefano <fulvio.notarstefano@gmail.com>
2018-05-08 23:35:08 +0800

    Expand test and specify WPBrowser admin path

--- tests/acceptance/AdminTestCest.php
+++ tests/acceptance/AdminTestCest.php
@@ -5,11 +5,13 @@ class AdminTestCest  {
 
 	public function TestPluginActivationDeactivation( AcceptanceTester $I ) {
 
-		$I->wantTo( 'See if WP PHP Console is listed in Plugins page' );
+		$I->wantTo( 'See if WP PHP Console is listed in the Plugins page.' );
 
 		$I->amOnPluginsPage();
 
 		$I->seePluginActivated( 'wp-php-console' );
+
+		$I->wantTo( 'Deactivate and reactivate WP PHP Console.' );
 
 		$I->deactivatePlugin( 'wp-php-console' );
 		$I->activatePlugin( 'wp-php-console' );
