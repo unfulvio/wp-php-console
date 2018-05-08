cbea9f124a418f744792e68f6600ec7b9967418f
Fulvio Notarstefano <fulvio.notarstefano@gmail.com>
2018-05-08 23:32:26 +0800

    Write test case

--- tests/acceptance/AdminTestCest.php
+++ tests/acceptance/AdminTestCest.php
@@ -3,11 +3,16 @@
 class AdminTestCest  {
 
 
-	public function TestPluginsPage( AcceptanceTester $I ) {
+	public function TestPluginActivationDeactivation( AcceptanceTester $I ) {
 
 		$I->wantTo( 'See if WP PHP Console is listed in Plugins page' );
+
 		$I->amOnPluginsPage();
+
 		$I->seePluginActivated( 'wp-php-console' );
+
+		$I->deactivatePlugin( 'wp-php-console' );
+		$I->activatePlugin( 'wp-php-console' );
 	}
 
 
